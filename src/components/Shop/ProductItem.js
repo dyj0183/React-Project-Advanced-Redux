82b618b097e7940f90a9e90f2a5f5c30178a46cd
Yu-Chun Tung <dyj0183@byui.edu>
2022-02-21 15:59:17 -0700

    dispatch an action with new item data to add a new item into cart

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,23 +1,43 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+
+// import useDispatch
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+	const { id, title, price, description } = props;
 
-  return (
-    <li className={classes.item}>
-      <Card>
-        <header>
-          <h3>{title}</h3>
-          <div className={classes.price}>${price.toFixed(2)}</div>
-        </header>
-        <p>{description}</p>
-        <div className={classes.actions}>
-          <button>Add to Cart</button>
-        </div>
-      </Card>
-    </li>
-  );
+	const dispatch = useDispatch();
+
+	const addToCartHandler = () => {
+		// need to pass the new item data into this function
+		dispatch(
+			cartActions.addItemToCart({
+				id: id,
+				title: title,
+				price: price,
+				description: description,
+			})
+		);
+
+    console.log("Dispatch an action to add new item into cart!");
+	};
+
+	return (
+		<li className={classes.item}>
+			<Card>
+				<header>
+					<h3>{title}</h3>
+					<div className={classes.price}>${price.toFixed(2)}</div>
+				</header>
+				<p>{description}</p>
+				<div className={classes.actions}>
+					<button onClick={addToCartHandler}>Add to Cart</button>
+				</div>
+			</Card>
+		</li>
+	);
 };
 
 export default ProductItem;
