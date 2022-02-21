98d9b886c6a6939655a7549a5e1e1618d1f2a33a
YuChunRaceEntry <yuchun.raceentry@gmail.com>
2022-02-21 11:02:54 -0700

    set up createSlice for the ui component and configure the store

---
+++ src/store/ui-slice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const uiSlice = createSlice({
+	name: "ul",
+	initialState: { cartIsVisible: false },
+	reducers: {
+		toggle(state) {
+			// we NEVER want to mutate the old state! Even though it seems like we are mutating here,
+			// we are actually not because the redux toolkit is doing something for us behind the scene
+			state.cartIsVisible = !state.cartIsVisible;
+		},
+	},
+});
+
+// also want to export the actions
+export const uiActions = uiSlice.actions;
+
+export default uiSlice;
