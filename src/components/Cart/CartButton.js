a4be29c4ed8b4f48fa7b433f00913a2453c4409b
YuChunRaceEntry <yuchun.raceentry@gmail.com>
2022-02-21 11:52:19 -0700

    able to toggle the entire shopping cart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,12 +1,21 @@
-import classes from './CartButton.module.css';
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
+import { useDispatch } from "react-redux";
 
 const CartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
-  );
+	// get the access to dispatch function
+	const dispatch = useDispatch();
+
+	const toggleCartHandler = () => {
+		dispatch(uiActions.toggle());
+	};
+
+	return (
+		<button className={classes.button} onClick={toggleCartHandler}>
+			<span>My Cart</span>
+			<span className={classes.badge}>1</span>
+		</button>
+	);
 };
 
 export default CartButton;
